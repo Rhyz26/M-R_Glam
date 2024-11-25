96d4ee398ea42f1246e1a012239b43894e1d25da
Raymond Njagala <njagala.raymond@gmail.com>
2024-11-25 14:23:31 +0300

    Created components, installed dependecies

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Footer(){
+  return (
+    <div className='footer'>
+         <ul>
+         <li>
+
+         <Link to="/">Home</Link>
+         </li>
+         
+         
+         <li>
+         <Link to="/about">About us</Link>
+
+         </li>
+         
+         <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+
+            <li>
+                <Link to="/search">Search</Link>
+            </li>
+
+            <li>
+                <Link to="/account">My Account</Link>
+            </li>
+
+            <li>
+                <Link to="/deal">Deals</Link>
+            </li>
+
+            <li>
+                <Link to="/categories">Categories</Link>
+            </li>
+
+        </ul>
+
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li className="text-orange-600">
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About us</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+
+        <li className='text-red-600'>
+          <Link to="/search">Search</Link>
+        </li>
+
+        <li>
+          <Link to="/deal">Deals</Link>
+        </li>
+
+        <li>
+          <Link to="/categories">Categories</Link>
+        </li>
+
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
