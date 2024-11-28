f0ff3a46a13601ad17f382f107ebd90bfcdd7ea4
Melissa9mpenzi <melissampenzi@gmail.com>
2024-11-28 09:32:19 +0300

    added styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,93 +1,104 @@
-function Footer(){
+import { Link } from 'react-router-dom';
+
+function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-section">
-        <h3>Customer Service</h3>
-        <ul>
-          <li><a href="/help-center">Help Center</a></li>
-          <li><a href="/terms">Terms and Conditions</a></li>
-          <li><a href="/feedback">Take our feedback survey</a></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h3>Shopping with Us</h3>
-        <ul>
-          <li><a href="/payments">Making Payments</a></li>
-          <li><a href="/delivery">Delivery Options</a></li>
-          <li><a href="/buyer-protection">Buyer Protection</a></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h3>Collaborate with Us</h3>
-        <ul>
-          <li><a href="/partnerships">Partnerships</a></li>
-          <li><a href="/affiliates">Affiliate Program</a></li>
-          <li><a href="/seller-login">Seller Log In</a></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h3>PAYMENT METHODS</h3>
-        <div className="payment-icons flex">
-          <img src="/public/visa.png" alt="Visa" />
-          <img src="/mastercard.png" alt="MasterCard" />
-          <img src="/public/paypal.png" alt="PayPal" />
-          <img src="/apple_pay.png" alt="Apple Pay" />
-          <img src="/airtel_momo.png" alt="Airtel Pay" />
-          <img src="/mtn_momo.png" alt="MTN Pay" />
+    <footer
+      style={{
+        backgroundColor: '#fde8d7',
+        color: '#bb2c13',
+        padding: '20px 0',
+        position: 'absolute',
+        bottom: '0',
+        width: '100%',
+        textAlign: 'center',
+        boxSizing: 'border-box',
+      }}
+    >
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div>
+          <h3 style={{ color: '#bb2c13' }}>CUSTOMER SERVICE</h3>
+          <Link to="/help-center" style={linkStyle}>
+            Help Center
+          </Link>
+          <Link to="/terms" style={linkStyle}>
+            Terms and Conditions
+          </Link>
+          <Link to="/feedback" style={linkStyle}>
+            Take our Feedback Survey
+          </Link>
         </div>
-      </div>
-      <div className="footer-section">
-        <h3 className="mr-0">Stay Connected</h3>
-        <div className="social-icons flex">
-          <a href="https://facebook.com"><i className="fab fa-facebook"></i><img src="/public/facebook.jpg" alt="facebook" /></a>
-          <a href="https://twitter.com"><i className="fab fa-twitter"></i><img src="/public/x space.png" alt="twitter" /></a>
-          <a href="https://instagram.com"><i className="fab fa-instagram"><img src="/public/instagram.jpg" alt="instagram" /></i></a>
-          <a href="https://pinterest.com"><i className="fab fa-instagram"><img src="/public/pinterest.png" alt="pinterest" /></i></a>
-          
+
+        <div>
+          <h3 style={{ color: '#bb2c13' }}>SHOP WITH US</h3>
+          <Link to="/payments" style={linkStyle}>
+            Making Payments
+          </Link>
+          <Link to="/delivery" style={linkStyle}>
+            Delivery Options
+          </Link>
+          <Link to="/buyer-protection" style={linkStyle}>
+            Buyer Protection
+          </Link>
+        </div>
+
+        <div>
+          <h3 style={{ color: '#bb2c13' }}>COLLABORATE WITH US</h3>
+          <Link to="/partnerships" style={linkStyle}>
+            Partnerships
+          </Link>
+          <Link to="/affiliates" style={linkStyle}>
+            Affiliate Program
+          </Link>
+          <Link to="/seller-login" style={linkStyle}>
+            Seller Log In
+          </Link>
+        </div>
+
+        <div>
+          <h3 style={{ color: '#bb2c13' }}>PAYMENT METHODS</h3>
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+            <img src="/visa.png" alt="Visa" style={iconStyle} />
+            <img src="/mastercard.png" alt="MasterCard" style={iconStyle} />
+            <img src="/paypal.png" alt="PayPal" style={iconStyle} />
+            <img src="/apple_pay.png" alt="Apple Pay" style={iconStyle} />
+            <img src="/airtel_momo.png" alt="Airtel Pay" style={iconStyle} />
+            <img src="/mtn_momo.png" alt="MTN Pay" style={iconStyle} />
+          </div>
+        </div>
+
+        <div>
+          <h3 style={{ color: '#bb2c13' }}>STAY CONNECTED</h3>
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+            <a href="https://facebook.com">
+              <img src="/facebook.jpg" alt="Facebook" style={iconStyle} />
+            </a>
+            <a href="https://twitter.com">
+              <img src="/x space.png" alt="Twitter" style={iconStyle} />
+            </a>
+            <a href="https://instagram.com">
+              <img src="/instagram.jpg" alt="Instagram" style={iconStyle} />
+            </a>
+            <a href="https://pinterest.com">
+              <img src="/pinterest.png" alt="Pinterest" style={iconStyle} />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
   );
+}
+
+const linkStyle = {
+  color: '#f8a879',
+  textDecoration: 'none',
+  margin: '5px 0',
+  fontWeight: 'bold',
+  display: 'block',
 };
-export default Footer
 
-//     <div className='footer'>
-//          <ul>
-//          <li>
+const iconStyle = {
+  width: '40px',
+  height: 'auto',
+};
 
-//          <Link to="/">Home</Link>
-//          </li>
-         
-         
-//          <li>
-//          <Link to="/about">About us</Link>
-
-//          </li>
-         
-//          <li>
-//                 <Link to="/contact">Contact</Link>
-//             </li>
-
-//             <li>
-//                 <Link to="/search">Search</Link>
-//             </li>
-
-//             <li>
-//                 <Link to="/account">My Account</Link>
-//             </li>
-
-//             <li>
-//                 <Link to="/deal">Deals</Link>
-//             </li>
-
-//             <li>
-//                 <Link to="/categories">Categories</Link>
-//             </li>
-
-//         </ul>
-
-
-//     </div>
-  
-
-// export default Footer
+export default Footer;
