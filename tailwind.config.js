2e4cebc31244e8531ed94de187e0145c895437a7
Melissa9mpenzi <melissampenzi@gmail.com>
2024-11-28 14:21:42 +0300

    updated contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+ export default {
+   content: [
+     "./index.html",
+     "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+     extend: { colors: {
+        peach: '#fbceb1',
+        apricot: '#f8a879',
+        orange: '#f47a43',
+        tangerine: '#f0571f',
+        red: '#e23d14',
+        deepRed: '#bb2c13',
+        maroon: '#952517',
+        wine: '#782116',
+      },},
+   },
+   plugins: [],
+ }
+
+
+
 
 
